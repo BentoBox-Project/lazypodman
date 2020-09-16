c530c00d124a019f8fa1969c42dbfbe441b2f2cd
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-09-15 22:44:35 -0500

    feat(podman): create function types to decouple the dependencies on the actual functions from podman/bindings

--- pkg/podman/podman.go
+++ pkg/podman/podman.go
@@ -5,10 +5,7 @@ import (
 	"os"
 
 	"github.com/containers/libpod/v2/pkg/bindings"
-	"github.com/containers/libpod/v2/pkg/bindings/containers"
-	"github.com/containers/libpod/v2/pkg/bindings/images"
-	"github.com/containers/libpod/v2/pkg/bindings/pods"
-	"github.com/containers/libpod/v2/pkg/bindings/volumes"
+	"github.com/containers/libpod/v2/pkg/domain/entities"
 )
 
 // Podman struct
@@ -17,6 +14,20 @@ type Podman struct {
 	ComposeFile string
 }
 
+// Internal functions types used to mock the real ones from podman/bindings package
+
+// PodsList is the function type with the same signature of pods.List
+type PodsList func(ctx context.Context, filters map[string][]string) ([]*entities.ListPodsReport, error)
+
+// ContainersList is the function type with the same signature of containers.List
+type ContainersList func(ctx context.Context, filters map[string][]string, all *bool, last *int, pod, size, sync *bool) ([]entities.ListContainer, error)
+
+// ImagesList is the function type with the same signature of images.List
+type ImagesList func(ctx context.Context, all *bool, filters map[string][]string) ([]*entities.ImageSummary, error)
+
+// VolumesList is the function type with the same signature of volumes.List
+type VolumesList func(ctx context.Context, filters map[string][]string) ([]*entities.VolumeListReport, error)
+
 // APIConn returns an Podman V2 API connection as a context.Context
 func APIConn() (context.Context, error) {
 	// Get Podman socket location
@@ -24,17 +35,17 @@ func APIConn() (context.Context, error) {
 	socket := "unix:" + sockDir + "/podman/podman.sock"
 
 	// Connect to Podman socket
-	connText, err := bindings.NewConnection(context.Background(), socket)
+	ctx, err := bindings.NewConnection(context.Background(), socket)
 	if err != nil {
 		return nil, err
 	}
 
-	return connText, nil
+	return ctx, nil
 }
 
 // Pods returns a slice of strings with the name of the active pods
-func (p *Podman) Pods(conn context.Context) ([]string, error) {
-	podList, err := pods.List(conn, nil)
+func (p *Podman) Pods(ctx context.Context, plist PodsList) ([]string, error) {
+	podList, err := plist(ctx, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -49,10 +60,10 @@ func (p *Podman) Pods(conn context.Context) ([]string, error) {
 }
 
 // Containers retuns a slice of strings with the names of the active containers or those listted on a docker-compose file
-func (p *Podman) Containers(conn context.Context) ([]string, error) {
+func (p *Podman) Containers(ctx context.Context, clist ContainersList) ([]string, error) {
 	var latestContainers = 10
 
-	containerList, err := containers.List(conn, nil, nil, &latestContainers, nil, nil, nil)
+	containerList, err := clist(ctx, nil, nil, &latestContainers, nil, nil, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -66,9 +77,9 @@ func (p *Podman) Containers(conn context.Context) ([]string, error) {
 }
 
 // Images return the list of the current podman images in the system
-func (p *Podman) Images(conn context.Context) ([]string, error) {
+func (p *Podman) Images(ctx context.Context, iList ImagesList) ([]string, error) {
 	// List images
-	imageSummary, err := images.List(conn, nil, nil)
+	imageSummary, err := iList(ctx, nil, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -82,8 +93,8 @@ func (p *Podman) Images(conn context.Context) ([]string, error) {
 }
 
 // Volumes return the list of the current volumnes in the system
-func (p *Podman) Volumes(conn context.Context) ([]string, error) {
-	volumeList, err := volumes.List(conn, nil)
+func (p *Podman) Volumes(ctx context.Context, vList VolumesList) ([]string, error) {
+	volumeList, err := vList(ctx, nil)
 	if err != nil {
 		return nil, err
 	}
