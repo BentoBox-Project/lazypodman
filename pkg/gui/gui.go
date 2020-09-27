65b56ff7b0e31dfcd97a0015a7243a452b87eb4b
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-09-27 18:40:46 -0500

    feat(gui): improve the gui object to manage the functionality in a better way

--- pkg/gui/gui.go
+++ pkg/gui/gui.go
@@ -1,127 +1,168 @@
 package gui
 
 import (
-	"time"
+	"errors"
+	"os"
+	"os/exec"
 
-	"github.com/containers/libpod/v2/pkg/bindings/containers"
-	"github.com/containers/libpod/v2/pkg/bindings/images"
-	"github.com/containers/libpod/v2/pkg/bindings/pods"
-	"github.com/containers/libpod/v2/pkg/bindings/volumes"
+	"github.com/danvergara/lazypodman/pkg/commands"
 	"github.com/danvergara/lazypodman/pkg/podman"
-	ui "github.com/gizak/termui/v3"
-	"github.com/gizak/termui/v3/widgets"
+	"github.com/golang-collections/collections/stack"
+	"github.com/jesseduffield/gocui"
 	"github.com/sirupsen/logrus"
-	terminal "github.com/wayneashleyberry/terminal-dimensions"
 )
+
+// SentinelErrors are the errors that have special meaning and need to be checked
+// by calling functions. The less of these, the better
+type SentinelErrors struct {
+	ErrSubProcess   error
+	ErrNoContainers error
+	ErrNoImages     error
+	ErrNoVolumes    error
+}
+
+// GenerateSentinelErrors makes the sentinel errors for the gui. We're defining it here
+// because we can't do package-scoped errors with localization, and also because
+// it seems like package-scoped variables are bad in general
+// https://dave.cheney.net/2017/06/11/go-without-package-scoped-variables
+// In the future it would be good to implement some of the recommendations of
+// that article. For now, if we don't need an error to be a sentinel, we will just
+// define it inline. This has implications for error messages that pop up everywhere
+// in that we'll be duplicating the default values. We may need to look at
+// having a default localisation bundle defined, and just using keys-only when
+// localising things in the code.
+func (gui *Gui) GenerateSentinelErrors() {
+	gui.Errors = SentinelErrors{
+		ErrSubProcess:   errors.New("running subprocess"),
+		ErrNoContainers: errors.New("No containers"),
+		ErrNoImages:     errors.New("No Images"),
+		ErrNoVolumes:    errors.New("No volumes"),
+	}
+}
 
 // Gui wrapes the gocui object which handles rendering and events
 type Gui struct {
-	Grid          *ui.Grid
+	g             *gocui.Gui
+	Errors        SentinelErrors
+	Log           *logrus.Entry
 	PodmanBinding *podman.Podman
-	Pods          *widgets.List
-	Services      *widgets.List
-	Images        *widgets.List
-	Volumes       *widgets.List
+	State         guiState
+	CyclableViews []string
+	SubProcess    *exec.Cmd
+	OSCommand     *commands.OSCommand
+}
+
+type servicePanelState struct {
+	SelectedLine int
+	ContextIndex int // for specifying if you are looking at logs/stats/config/etc
+}
+
+type containerPanelState struct {
+	SelectedLine int
+	ContextIndex int // for specifying if you are looking at logs/stats/config/etc
+}
+
+type podPanelState struct {
+	SelectedLine int
+	ContextIndex int // for specifying if you are looking at logs/stats/config/etc
+}
+
+type projectState struct {
+	ContextIndex int // for specifying if you are looking ate credits/logs
+}
+
+type menuPanelState struct {
+	SelectedLine int
+	OnPress      func(*gocui.Gui, *gocui.View) error
+}
+
+type mainPanelState struct {
+	// ObjectKey tells us what context we are in. For example, if we are looking at the logs a particular services in the services panel this key might be 'services-<service id>-logs'. The key is made so that if something changes which might require us to re-run the logs command or run a different command, the key will be different, and we'll then know to do whatever is required. Object key probably isn't the best name for this but Context is already used to refer to tabs. Maybe I should just call them tabs.
+	ObjectKey string
+}
+
+type imagePanelState struct {
+	SelectedLine int
+	ContextIndex int // for specifying if you are looking at logs/stats/config/etc
+}
+
+type volumePanelState struct {
+	SelectedLine int
+	ContextIndex int // for specifying if you are looking at logs/stats/config/etc
+}
+
+type panelState struct {
+	Pods       *podPanelState
+	Services   *servicePanelState
+	Containers *containerPanelState
+	Menu       *menuPanelState
+	Main       *mainPanelState
+	Images     *imagePanelState
+	Volumes    *volumePanelState
+	Project    *projectState
+}
+
+type guiState struct {
+	MenuItemCount int
+	Panels        *panelState
+	PreviousViews *stack.Stack
+	SessionIndex  int
 }
 
 // NewGui returns a new Gui object
 func NewGui(composeFile string) *Gui {
+	initalState := guiState{
+		Panels: &panelState{
+			Pods:       &podPanelState{SelectedLine: -1},
+			Containers: &containerPanelState{SelectedLine: -1},
+			Images:     &imagePanelState{SelectedLine: -1},
+			Volumes:    &volumePanelState{SelectedLine: -1},
+			Menu:       &menuPanelState{},
+			Main: &mainPanelState{
+				ObjectKey: "",
+			},
+			Project: &projectState{},
+		},
+		PreviousViews: stack.New(),
+	}
+
 	gui := &Gui{
-		Grid:     ui.NewGrid(),
-		Pods:     widgets.NewList(),
-		Services: widgets.NewList(),
-		Images:   widgets.NewList(),
-		Volumes:  widgets.NewList(),
 		PodmanBinding: &podman.Podman{
 			ComposeFile: composeFile,
 		},
+		State:         initalState,
+		CyclableViews: []string{"project", "pod", "containers", "images", "volumes"},
 	}
 
-	gui.initUI()
+	gui.GenerateSentinelErrors()
 
 	return gui
 }
 
-func (g *Gui) initUI() {
-	g.Pods.Title = "Pods"
-	g.Services.Title = "Services"
-	g.Images.Title = "Images"
-	g.Volumes.Title = "Volumes"
-
-	g.Grid.Set(
-		ui.NewCol(.3,
-			ui.NewRow(1.0/4, g.Pods),
-			ui.NewRow(1.0/4, g.Services),
-			ui.NewRow(1.0/4, g.Images),
-			ui.NewRow(1.0/4, g.Volumes),
-		),
-	)
-	width, _ := terminal.Width()
-	height, _ := terminal.Height()
-
-	g.Grid.SetRect(0, 0, int(width), int(height))
-}
-
-// render display the Grid on the terminal
-func (g *Gui) render() {
-	ctx, err := podman.APIConn()
-	if err != nil {
-		logrus.Error(err)
-	}
-
-	podNames, err := g.PodmanBinding.Pods(ctx, pods.List)
-
-	if err != nil {
-		logrus.Error(err)
-	}
-
-	cNames, err := g.PodmanBinding.Containers(ctx, containers.List)
-
-	if err != nil {
-		logrus.Error(err)
-	}
-
-	imageNames, err := g.PodmanBinding.Images(ctx, images.List)
-	if err != nil {
-		logrus.Error(err)
-	}
-
-	vNames, err := g.PodmanBinding.Volumes(ctx, volumes.List)
-	if err != nil {
-		logrus.Error(err)
-	}
-
-	g.Pods.Rows = podNames
-	g.Services.Rows = cNames
-	g.Images.Rows = imageNames
-	g.Volumes.Rows = vNames
-
-	ui.Render(g.Grid)
-}
-
 // Run run the cli graphic interface
-func (g *Gui) Run() error {
-
-	if err := ui.Init(); err != nil {
+func (gui *Gui) Run() error {
+	g, err := gocui.NewGui(gocui.OutputNormal, false)
+	if err != nil {
 		return err
 	}
+	defer g.Close()
 
-	defer ui.Close()
+	g.Mouse = true
 
-	ev := ui.PollEvents()
-	//lint:ignore SA1015 we want to hide the actual implementation from the main package
-	tick := time.Tick(time.Second)
+	gui.g = g
 
-	for {
-		select {
-		case e := <-ev:
-			switch e.Type {
-			case ui.KeyboardEvent:
-				// quit on any keyboard event
-				return nil
-			}
-		case <-tick:
-			g.render()
-		}
+	g.SetManager(gocui.ManagerFunc(gui.layout), gocui.ManagerFunc(gui.getFocusLayout()))
+
+	if err := g.SetKeybinding("", []string{}, gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		// log.Panicln(err)
+		os.Exit(0)
+		return nil
 	}
+
+	err = g.MainLoop()
+	return err
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
