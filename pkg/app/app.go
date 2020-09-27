f7dbb354c724af9c31e61a118993ff713094373f
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-09-27 18:42:57 -0500

    refactor(pkg): remove the gui field from the config struct and pass it to the app one

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"github.com/danvergara/lazypodman/pkg/config"
+	"github.com/danvergara/lazypodman/pkg/gui"
 	"github.com/danvergara/lazypodman/pkg/logger"
 	"github.com/sirupsen/logrus"
 )
@@ -10,22 +11,20 @@ import (
 type App struct {
 	Config *config.Config
 	Log    *logrus.Entry
+	Gui    *gui.Gui
 }
 
 // NewApp boostrap a new application
-func NewApp(config *config.Config) (*App, error) {
+func NewApp(config *config.Config, composeFile string) (*App, error) {
 	app := &App{Config: config}
 	app.Log = logger.NewLogger(config)
-
+	app.Gui = gui.NewGui(composeFile)
 	return app, nil
 
 }
 
 // Run the application
 func (app *App) Run() error {
-	if err := app.Config.Gui.Run(); err != nil {
-		return err
-	}
-
-	return nil
+	err := app.Gui.RunWithSubprocesses()
+	return err
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,22 +1,18 @@
 package config
 
-import "github.com/danvergara/lazypodman/pkg/gui"
-
 // Config struct
 type Config struct {
 	Debug      bool
-	Gui        *gui.Gui
 	ProjectDir string
 	Name       string
 	Version    string
 }
 
 // NewConfig boostrap a new config for the application
-func NewConfig(name, composeFile, projectDir, version string) (*Config, error) {
+func NewConfig(name, projectDir, version string) (*Config, error) {
 	appConfig := &Config{
 		Name:       name,
 		ProjectDir: projectDir,
-		Gui:        gui.NewGui(composeFile),
 		Version:    version,
 	}
 
