2177f94dcc0bff44a55a64b56f3a1427299146ad
Daniel Omar Vergara Pérez <daniel.omar.vergara@gmail.com>
2020-10-11 00:11:15 -0500

    feat(main.go): replace the old main logic with Execute method from the root command

--- main.go
+++ main.go
@@ -1,67 +1,9 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/danvergara/lazypodman/pkg/app"
-	"github.com/danvergara/lazypodman/pkg/config"
-	"github.com/go-errors/errors"
+	"github.com/danvergara/lazypodman/cmd"
 )
-
-const (
-	appVersion   = "0.1.0"
-	versionUsage = "Prints current version"
-	fileUsage    = "Specify a alternate compose file"
-)
-
-var (
-	version     bool
-	composeFile string
-)
-
-func init() {
-	flag.BoolVar(&version, "version", false, versionUsage)
-	flag.BoolVar(&version, "v", false, versionUsage+" (shorthand)")
-	flag.StringVar(&composeFile, "file", "", fileUsage)
-	flag.StringVar(&composeFile, "f", "", fileUsage+" (shorthand)")
-}
 
 func main() {
-
-	flag.Parse()
-
-	if version {
-		fmt.Println(appVersion)
-		os.Exit(0)
-	}
-
-	projectDir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	if composeFile == "" {
-		composeFile = "docker-compose.yml"
-	}
-
-	appConfig, err := config.NewConfig("lazypodman", projectDir, "0.1.0")
-
-	if err != nil {
-		log.Fatal(fmt.Sprintf("%s\n", err.Error()))
-	}
-
-	app, err := app.NewApp(appConfig, composeFile)
-	if err == nil {
-		err = app.Run()
-	}
-
-	if err != nil {
-		newErr := errors.Wrap(err, 0)
-		stackTrace := newErr.ErrorStack()
-		app.Log.Error(stackTrace)
-		log.Fatal(fmt.Sprintf("%s\n", err.Error()))
-	}
+	cmd.Execute()
 }
